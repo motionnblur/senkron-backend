381599676eadad181d3b22f9ba89c1c89f732cc4
Can <carbonfiberdev@gmail.com>
2026-06-17 05:04:17 +0300

    first commit

---
+++ src/main/java/com/motionnblur/senkron_backend/SenkronBackendApplication.java
@@ -0,0 +1,13 @@
+package com.motionnblur.senkron_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenkronBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenkronBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motionnblur/senkron_backend/SenkronBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motionnblur.senkron_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenkronBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
